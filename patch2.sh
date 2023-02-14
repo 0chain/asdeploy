388ae21627c6809eea1e8960d1526b4ee99f352c
shahnawaz-creator <117025384+shahnawaz-creator@users.noreply.github.com>
2023-02-14 22:36:15 +0530

    updated patch.sh

--- patch2.sh
+++ patch2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sed -i "s|0chaindev/sharder:pr-1926-1a9bf524|0chaindev/sharder:pr-2109-2d796475|g" ~/sharder_deploy/docker.local/build.sharder/p0docker-compose.yaml
-sed -i "s|0chaindev/sharder:staging-02e43b7b|0chaindev/sharder:pr-2109-2d796475|g" ~/miner_deploy/docker.local/build.miner/p0docker-compose.yaml
+sed -i "s|0chaindev/miner:staging-02e43b7b|0chaindev/miner:pr-2109-2d796475|g" ~/miner_deploy/docker.local/build.miner/p0docker-compose.yaml
 cd ~/sharder_deploy/docker.local/sharder1
 ../bin/start.p0sharder.sh
 cd ~/miner_deploy/docker.local/miner1
